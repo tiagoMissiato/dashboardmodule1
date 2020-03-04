a7bb049488c4771cd1eab2c90515b65c52fe70d8
u001710 <tiago.rigoleto@xpi.com.br>
2020-03-04 15:36:45 -0300

    wip

--- featuremodule12/src/main/java/br/com/xpinc/mobile/featuremodule12/LoggerModule1.kt
+++ featuremodule12/src/main/java/br/com/xpinc/mobile/featuremodule12/LoggerModule1.kt
@@ -3,5 +3,5 @@ package br.com.xpinc.mobile.featuremodule12
 import android.util.Log
 
 object LoggerModule1 {
-    fun logModule1() = Log.i("LoggerModule1", "I am here uhull")
+    fun logModule1() = "I am here uhull"
 }
